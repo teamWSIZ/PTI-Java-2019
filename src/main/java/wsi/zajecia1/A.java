8ff17798c93577a22936848bcfef77d756633997
pmarecki <pmarecki@gmail.com>
2019-01-28 14:13:10 +0100

    prosty program Hello

--- src/main/java/wsi/zajecia1/A.java
+++ src/main/java/wsi/zajecia1/A.java
@@ -4,8 +4,8 @@ public class A {
     public static void main(String[] args) {
         System.out.println("cześć Hello!");
 
-        int x = 10;
-        int y = 12;
+        int x = 5;
+        int y = 5;
         System.out.println(x*y);        //120
 
         System.out.println("Krok " + y); //Krok 12
@@ -15,5 +15,20 @@ public class A {
         String c = a + " " + b;
         System.out.println(c);
         System.out.println(c.toUpperCase());
+
+        if (x == 10) {
+            System.out.println("x jest równe 10");
+        } else {
+            System.out.println("x nie jest równe 10");
+        }
+
+        if (x * y < 100) {
+            System.out.println("iloczyn x i y nie jest za duży");
+        }
+
+        if (x<10 && y<10) {
+            System.out.println("x i y są mniejsze od 10");
+        }
+
     }
 }
