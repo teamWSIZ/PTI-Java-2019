0157d2b594b12bbf318b3f6fbb09cc9afcdccf88
pmarecki <pmarecki@gmail.com>
2019-01-28 12:10:03 +0100

    init

---
+++ src/main/java/wsi/exec/http/MainController.java
@@ -0,0 +1,25 @@
+package wsi.exec.http;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wsi.exec.model.GenericResponse;
+
+/**
+ *
+ */
+
+
+@RestController
+@CrossOrigin
+@Slf4j
+public class MainController {
+
+    @GetMapping(value = "/status")
+    public GenericResponse appStatus() {
+        return new GenericResponse("App is running OK");
+    }
+
+}
