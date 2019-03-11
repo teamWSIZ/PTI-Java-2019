02856dfba239ae41b0297b4a0f06650761696353
Your Name <you@example.com>
2019-03-11 13:30:08 +0100

    wyjatki

--- src/main/java/wsi/zajecia4/Controller.java
+++ src/main/java/wsi/zajecia4/Controller.java
@@ -9,7 +9,13 @@ public class Controller {
     int x = 0;
     public void klikeeed() {
         x = x+1;
-        int liczbaN = Integer.valueOf(tf1.getText());
+        int liczbaN = 0;
+        String s = tf1.getText();
+        try {
+            liczbaN = Integer.valueOf(s);
+        } catch (NumberFormatException e) {
+            System.out.println("tu ma byc liczba a nie [" + s + "]");
+        }
         liczbaN += 10;
         System.out.println("przycisk został naciśnięty.. " + x + " razy");
         tf2.setText("" + liczbaN * (liczbaN+1)/2 );
