58e6921716ddcad1646ed60bc88617fb07fb54b7
Your Name <you@example.com>
2019-03-11 14:07:51 +0100

    rozcinanie napisów

---
+++ src/main/java/wsi/zajecia4/A.java
@@ -0,0 +1,20 @@
+package wsi.zajecia4;
+
+import java.util.Arrays;
+
+public class A {
+    public static void main(String[] args) {
+        //zadanie: pokroić napis typu csv na tablicę napisów
+        String s = "10,1,1,-1,0,12,-10";
+        String[] ss = s.split(","); //to jest tablica
+        for(String x : ss) {
+            System.out.println(x);
+        }
+        //wstęp do tablic w Java
+        System.out.println(Arrays.toString(ss));
+        System.out.println(ss.length);  //liczba elementow
+        System.out.println(ss[0]);  //odczyt pierwszego elementu
+        ss[1] = "111";  //przypisanie
+        System.out.println(Arrays.toString(ss));
+    }
+}
